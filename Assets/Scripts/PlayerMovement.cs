99865b839ade8fe2e8cf68b38ad23d7f5db16529
PR0PHET9 <jacobjetg@gmail.com>
2025-11-21 11:03:37 +1100

    Scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    public float speed; 
+    public float input; 
+    
+  
+    // Update is called once per frame
+    void Update()
+    {
+       input = Input.GetAxisRaw("Horizontal"); 
+    }
+
+    void FixedUpdate()
+    {
+        playerRb.linearVelocity = new Vector2 (input * speed, playerRb.linearVelocity.y);
+
+    }
+}
